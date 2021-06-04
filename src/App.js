e186c333637a252beaf803cab5b6e254f6cab7ea
king <2075259561@qq.com>
2021-06-04 18:07:20 +0800

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.less"
+import React, {Component} from "react";
+import {HashRouter as Router, Route, Link} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <ul>1111</ul>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App;
